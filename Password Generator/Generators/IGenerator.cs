1e5f0461106edca3fb225f216a32bc71ed05a00b
Iwan0129 <uzunow0129@gmail.com>
2020-11-30 03:17:55 +0200

    Small Code Rework

--- Password Generator/Generators/IGenerator.cs
+++ Password Generator/Generators/IGenerator.cs
@@ -5,10 +5,15 @@ namespace Password_Generator
     internal interface IGenerator<T> : IDisposable
     {
         bool UseBigLetters { get; set; }
+       
         bool UseDigits { get; set; }
+       
         bool UseLetters { get; set; }
+       
         bool UseSpace { get; set; }
+        
         bool UseSymbols { get; set; }
+       
         T Generate();
     }
 }
